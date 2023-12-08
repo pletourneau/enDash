44df8dd2f41b6ce89860a021e5627f0e5bc634f7
Paul LeTourneau <pletourneau62@gmail.com>
2023-12-07 17:35:07 -0800

    found workaround, will code tomorrow

--- endash/src/components/Login.js
+++ endash/src/components/Login.js
@@ -6,12 +6,12 @@ const Login = () => {
     // Load environment variables from .env file
     const clientId = process.env.REACT_APP_CLIENT_ID;
     const authUri = process.env.REACT_APP_AUTH_URI;
-    // const redirectUri = process.env.REACT_APP_REDIRECT_URI;
+    const redirectUri = process.env.REACT_APP_REDIRECT_URI;
 
-    const redirectUri =
-      "https://api.enphaseenergy.com/oauth/authorize?response_type=code&client_id=192ef4c2b2f65ae0e67eb93eb402e430&redirect_uri=https://api.enphaseenergy.com/oauth/redirect_uri";
-    // const authorizationUrl = `https://api.enphaseenergy.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
-    const authorizationUrl = redirectUri;
+    // const redirectUri =
+    //   "https://api.enphaseenergy.com/oauth/authorize?response_type=code&client_id=192ef4c2b2f65ae0e67eb93eb402e430&redirect_uri=https://api.enphaseenergy.com/oauth/redirect_uri";
+    const authorizationUrl = `https://api.enphaseenergy.com/oauth/authorize?response_type=code&client_id=192ef4c2b2f65ae0e67eb93eb402e430&redirect_uri=http://localhost:3000/callback `;
+    // const authorizationUrl = redirectUri;
 
     console.log("clientId:", clientId);
     console.log("authUri:", authUri);
