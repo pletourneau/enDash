4f1e162b585d10c3dd83e29d1c56ce0756cc58a8
Paul LeTourneau <pletourneau62@gmail.com>
2023-12-08 14:06:01 -0800

    access env variables correctly

--- endash/server.js
+++ endash/server.js
@@ -1,8 +1,8 @@
 // server.js
+require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-// require("dotenv").config();
 
 const app = express();
 const port = 3001;
@@ -14,10 +14,7 @@ app.post("/oauth/token", async (req, res) => {
   const { code } = req.body;
   console.log("Authorization Code:", code);
 
-  // const clientId = process.env.REACT_APP_CLIENT_ID;
-  // const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
   const base64 = process.env.REACT_APP_BASE_64;
-  // const redirectUri = process.env.REACT_APP_REDIRECT_URI;
   const redirectUri = "https://api.enphaseenergy.com/oauth/redirect_uri";
   try {
     const params = new URLSearchParams();
@@ -25,12 +22,8 @@ app.post("/oauth/token", async (req, res) => {
     params.append("code", code);
     params.append("redirect_uri", redirectUri);
 
-    const authHeader = base64;
-    console.log(base64);
-    // console.log(
-    //   "Base64 from environment variable:",
-    //   process.env.REACT_APP_BASE_64
-    // );
+    const authHeader = `Basic ${base64}`;
+    console.log(authHeader);
     console.log("Authorization Header:", authHeader);
     const response = await axios.post(
       "https://api.enphaseenergy.com/oauth/token",
@@ -43,11 +36,8 @@ app.post("/oauth/token", async (req, res) => {
       }
     );
 
-    // console.log("Token Exchange Response:", response.data);
-
     res.json(response.data);
   } catch (error) {
-    // console.error("Error exchanging authorization code:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
