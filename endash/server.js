1dce0e1a562afad4a28399aea52320655d31e652
Paul LeTourneau <pletourneau62@gmail.com>
2023-12-08 17:34:12 -0800

    clean up console logs

--- endash/server.js
+++ endash/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.post("/oauth/token", async (req, res) => {
   const { code } = req.body;
-  console.log("Authorization Code:", code);
+  // console.log("Authorization Code:", code);
 
   const base64 = process.env.REACT_APP_BASE_64;
   const redirectUri = "https://api.enphaseenergy.com/oauth/redirect_uri";
@@ -25,7 +25,7 @@ app.post("/oauth/token", async (req, res) => {
 
     const authHeader = `Basic ${base64}`;
 
-    console.log("Authorization Header:", authHeader);
+    // console.log("Authorization Header:", authHeader);
     const response = await axios.post(
       "https://api.enphaseenergy.com/oauth/token",
       params.toString(),
@@ -36,36 +36,41 @@ app.post("/oauth/token", async (req, res) => {
         },
       }
     );
-    console.log(response);
+
     const accessToken = response.data.access_token;
 
     // Use accessToken as the key for better clarity
     tokenStore[accessToken] = code;
     res.json(response.data);
   } catch (error) {
+    // console.error("Error fetching access token:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 app.get("/api/system-summary", async (req, res) => {
   try {
-    const systemId = process.env.REACT_APP_SYSTEMID;
+    // const systemId = process.env.REACT_APP_SYSTEMID;
+    const key = process.env.REACT_APP_API_KEY;
     const accessToken = req.query.code; // Rename code to accessToken for consistency
 
-    console.log("Token Store:", tokenStore);
-    if (!accessToken) {
-      throw new Error("Access token not provided");
-    }
+    // console.log("Token Store:", tokenStore);
+    // if (!accessToken) {
+    //   throw new Error("Access token not provided");
+    // }
 
     const storedCode = tokenStore[accessToken];
-    console.log("Authorization code retrieved from tokenStore:", storedCode);
 
     if (!storedCode) {
       throw new Error("Authorization code not found");
     }
 
+    // console.log("Request Headers:", {
+    //   Authorization: `Bearer ${accessToken}`,
+    // });
+
     const response = await axios.get(
-      `https://api.enphaseenergy.com/api/V4/systems/?key=${systemId}`,
+      `https://api.enphaseenergy.com/api/V4/systems/?key=${key}`,
       {
         headers: {
           Authorization: `Bearer ${accessToken}`,
@@ -74,8 +79,9 @@ app.get("/api/system-summary", async (req, res) => {
     );
 
     res.json(response.data);
+    console.log(response.data.message);
   } catch (error) {
-    console.error("Error fetching system summary:", error);
+    // console.error("Error fetching system summary:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
