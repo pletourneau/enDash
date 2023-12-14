412e7d2ee04b0fb975f1d75a867884282f53196b
Paul LeTourneau <pletourneau62@gmail.com>
2023-12-13 16:59:43 -0800

    add config for heroku

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 const tokenStore = {};
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/build/index.html"));
+});
 
 app.use(express.json());
 app.use(cors());
