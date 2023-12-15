421efb002b7a8bf33decaeb440b7234b8a7fe23c
Paul LeTourneau <pletourneau62@gmail.com>
2023-12-15 11:30:01 -0800

    add if to login for valid access token

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,20 +2,33 @@
 import React from "react";
 import axios from "axios";
 
-// const exp = localStorage.getItem("expires_at");
-// console.log(exp);
-// const now = Math.floor(Date.now() / 1000);
-// console.log(now);
 const Login = () => {
+  const accessToken = localStorage.getItem("access_token");
+  const now = Math.floor(Date.now() / 1000);
+  const exp = localStorage.getItem("expires_at");
+  console.log(exp);
   const handleLogin = async () => {
-    try {
-      // Make a request to the server to initiate the OAuth flow
-      const response = await axios.get("/initiate-oauth");
-      console.log(response);
-      // Redirect the user to the authorization URL provided by the server
-      window.location.href = response.data.authorizationUrl;
-    } catch (error) {
-      console.error("Error initiating OAuth flow:", error);
+    if (now < exp) {
+      const response = await axios.get("/api/system-summary", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          key: process.env.REACT_APP_API_KEY,
+        },
+        params: {
+          end_at: now,
+        },
+      });
+      setData(response.data);
+    } else {
+      try {
+        // Make a request to the server to initiate the OAuth flow
+        const response = await axios.get("/initiate-oauth");
+        console.log(response);
+        // Redirect the user to the authorization URL provided by the server
+        window.location.href = response.data.authorizationUrl;
+      } catch (error) {
+        console.error("Error initiating OAuth flow:", error);
+      }
     }
   };
 
