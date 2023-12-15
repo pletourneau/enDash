f12bb5a45488b18c7f23ab0052673216ca6f65ce
Paul LeTourneau <pletourneau62@gmail.com>
2023-12-15 11:47:54 -0800

    amend oauth flow

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,24 +3,25 @@ import React from "react";
 import axios from "axios";
 
 const Login = () => {
-  const accessToken = localStorage.getItem("access_token");
+  // Retrieve the expiration time from localStorage
+  const exp = parseInt(localStorage.getItem("expires_at"), 10);
   const now = Math.floor(Date.now() / 1000);
-  const exp = localStorage.getItem("expires_at");
-  console.log(exp);
+
+  // Redirect to the dashboard if the token hasn't expired yet
+  if (now < exp) {
+    window.location.href = "/dashboard";
+    return null; // Render nothing for this component
+  }
+
   const handleLogin = async () => {
-    if (now < exp) {
-      window.location.href = "/dashboard";
-      return;
-    } else {
-      try {
-        // Make a request to the server to initiate the OAuth flow
-        const response = await axios.get("/initiate-oauth");
-        console.log(response);
-        // Redirect the user to the authorization URL provided by the server
-        window.location.href = response.data.authorizationUrl;
-      } catch (error) {
-        console.error("Error initiating OAuth flow:", error);
-      }
+    try {
+      // Make a request to the server to initiate the OAuth flow
+      const response = await axios.get("/initiate-oauth");
+      console.log(response);
+      // Redirect the user to the authorization URL provided by the server
+      window.location.href = response.data.authorizationUrl;
+    } catch (error) {
+      console.error("Error initiating OAuth flow:", error);
     }
   };
 
